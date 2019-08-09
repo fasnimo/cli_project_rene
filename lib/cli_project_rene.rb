ad48bce30d5ec7784c8a3259b3b605302656d849
Rene Cocom <fasnimo@gmail.com>
2019-08-09 01:34:56 +0000

    Fist commit

---
+++ lib/cli_project_rene.rb
@@ -0,0 +1,6 @@
+require "cli_project_rene/version"
+#environment
+module CliProjectRene
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_project_rene/version.rb
@@ -0,0 +1,3 @@
+module CliProjectRene
+  VERSION = "0.1.0"
+end
