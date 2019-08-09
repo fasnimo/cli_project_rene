681d79707d428f758a87f3ec1dbc78343eca5d1a
Rene Cocom <fasnimo@gmail.com>
2019-08-09 17:43:15 +0000

    changes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,11 @@ class CliProjectRene::Scraper
 
   def self.scraper_resturants
       doc = Nokogiri::HTML(open("https://www.tasteofhome.com/collection/best-airport-restaurants/"))
-      list = doc.search("div.listicle-page.tracking-fired a")
+      list = doc.search("div.listicle-page-container")
 
       name_list = list.search("h4").map{|h4| h4.text.strip}
       name = name_list.uniq
-      url_last = list.search("a").map{|s| s.attr("href")}
+      url_last = list.search("a").map{|search| search.attr("href")}
       location = returants.css("p.stong").text
       name.each.with_index do |name, index|
       resturant = Airport.new(name[index], location)
