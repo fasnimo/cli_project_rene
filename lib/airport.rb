4e4e8b4a7d4fc02b27f0e9848e92f4603dbbdf6f
Rene Cocom <fasnimo@gmail.com>
2019-08-10 01:51:36 +0000

    Add constence

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,12 @@
 class Airport
-  attr_accessor :name, :url_last
+  attr_accessor :name, :url_last, :info
 
   @@all = []
 
-  def initialize(name=nil, url_last= nil)
+  def initialize(name=nil, url_last= nil, info = info)
     @name = name
     @url_last = url_last
+    @info = info
     @@all << self
   end
 
@@ -16,7 +17,8 @@ class Airport
   def self.new_list(l)
     self.new(
     l.css('h4').collect{|s| s.css('strong').text.strip},
-    l.css('h4 a').attribute('href')
+    l.css('h4 a').attribute('href'),
+    l.css('p')[2]
     # "#{l.css('h4').collect{|d| d.css('a.SWhtmlLink').attribute('href')}}"
     )
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,37 +4,66 @@ class Cli
 
   def run
     Scraper.new.list_airport
-  
-    list_resturants
-    # self.welcome
-    # choose_airport
-    # display_links
+    Scraper.new.scrape_index
+    puts "Welcome the Best Airports Resturant Foodies Guide!"
+    fly
   end
 
-  def welcome
-    puts "Welcome!\n\n"
+  def fly
+    puts "\n\n Would you like to view the first or last of the list?"
+    puts "(please enter: 'first' or 'last')"
+    input = gets.strip.downcase
+
+    if input == "first"
+      input = 1
+      the_airports(input)
+    elsif input == "bottom"
+      input = 11
+      the_airports(input)
+  else
+    puts "\nPlease enter your preference."
+    fly
   end
 
-  def list_resturants
-    Airport.all[0,20].each do |r|
-    puts "#{r.name.join} \n #{r.url_last}"
+  puts "(Enter a number from 1 - 20)"
+  input = gets.strip.to_i
+
+  if input >= 1 && input > 20
+    resturant_info(input)
+  else
+    while input < 1 || input > 20
+    puts "Please enter a valid number from 1-20"
+    input = gets.strip.to_i
   end
-    # puts "What are you in the mood for?"
-    # Airport.all.take(20).each_with_index {|res, index| puts "#{index +1}. #{res.name}"}
+    resturant_info(input)
+end
+
+  puts "Are you ready to venture off on new adventures? Y/N"
+  if input == "y"
+    fly
+  else
+    puts "\n\n Thank you!"
+    exit
+  end
+end
+
+  def the_airports(place)
+    if place == 1
+      puts "\n\n Beginning list of Best Airport Resturants \n\n"
+      Airport.all[place - 10].each do |p|
+        puts "#{p.name}. #{p.url_last}"
+      end
+    else
+      puts "\n\n Ending of list of Best Airport Resturants \n\n"
+      Airport.all[place - 10].each do |p|
+        puts "#{p.name}. #{p.url_last}"
+      end
+  end
+end
+
+  def resturant_info(info)
+    puts ""
+
   end
 
-  def choose_airport
-    puts "Choose a location"
-    index = gets.strip.to_i - 1
-    resturant = Airport.all[index]
-    Scraper.scrape_name(resturant)
-    self.display_links(resturant)
-  end
-  #
-  # def display_links(resturant)
-  #   puts "\n\n\n"
-  #   puts resturant.name
-  #   puts resturant.location
-  #   puts "\n\n\n"
-  # end
 end
